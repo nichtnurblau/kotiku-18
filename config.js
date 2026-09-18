e8508e0d45e559e18a85b1184808ed59070c410f
kevin <78381249+nichtnurblau@users.noreply.github.com>
2026-09-18 23:42:57 +0300

    Add birthday video and update greeting

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// Укажите ссылку на видео либо положите birthday.mp4 рядом с index.html
-// и замените пустую строку ниже на "birthday.mp4".
-window.BIRTHDAY_VIDEO = "";
+window.BIRTHDAY_VIDEO = "birthday.mp4";
